9d2cadd228053b276c2c91e6e53773ebe64a064d
Jürgen Rühle <j-r@online.de>
2025-01-15 17:38:04 +0100

    Improve leads API and documentation
    Refactor code to expose more functionality to mods and improve readability.

--- mods/ENTITIES/mcl_boats/init.lua
+++ mods/ENTITIES/mcl_boats/init.lua
@@ -250,9 +250,7 @@ function boat:on_activate(staticdata)
 		self._v = data.v
 		self._last_v = self._v
 		self._itemstring = data.itemstring
-		self.leader = data.leader
-		self.leadermob = data.leadermob
-		self.tied_to_node = data.tied_to_node
+		self._lead_leader = data._lead_leader
 
 		-- Fall back to oak boat texture if no texture is set
 		if not data.textures then
@@ -294,9 +292,7 @@ function boat:get_staticdata()
 		itemstring = self._itemstring,
 		textures = props and props.textures or nil,
 		variant = self._variant,
-		leader = self.leader,
-		leadermob = self.leadermob,
-		tied_to_node = self.tied_to_node,
+		_lead_leader = self._lead_leader,
 	})
 end
 
