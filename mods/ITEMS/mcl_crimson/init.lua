c796be4dab5a001e9efd69b42063be0cb0df1a97
xox <abc@example.com>
2026-01-16 21:09:09 +0100

    Rename to `mcl_shelves`

--- mods/ITEMS/mcl_crimson/init.lua
+++ mods/ITEMS/mcl_crimson/init.lua
@@ -122,7 +122,7 @@ mcl_trees.register_wood("crimson",{
 		_mcl_cooking_output = ""
 	},
 	shelf = {
-		tiles = mcl_shelfs.sliced_shelf_texture("mcl_crimson_crimson_shelf.png"),
+		tiles = mcl_shelves.sliced_shelf_texture("mcl_crimson_crimson_shelf.png"),
 		groups = {fire_flammability = 0, fire_encouragement = 0}
 	},
 	wood = {
@@ -190,7 +190,7 @@ mcl_trees.register_wood("warped",{
 		_mcl_cooking_output = ""
 	},
 	shelf = {
-		tiles = mcl_shelfs.sliced_shelf_texture("mcl_crimson_warped_shelf.png"),
+		tiles = mcl_shelves.sliced_shelf_texture("mcl_crimson_warped_shelf.png"),
 		groups = {fire_flammability = 0, fire_encouragement = 0}
 	},
 	wood = {
