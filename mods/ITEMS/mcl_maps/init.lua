4e0ace5053d92ece24e4816ef43105f1ad1671ef
Po Lu <hbm78z8ub@mozmail.com>
2026-06-25 02:00:18 +0800

    Give mcl_use_old_map_grid precedence over existence of old map textures
    * init.lua (mcl_maps): As stated in title.

--- mods/ITEMS/mcl_maps/init.lua
+++ mods/ITEMS/mcl_maps/init.lua
@@ -25,12 +25,14 @@ end
 local use_old_map_grid
 	= core.get_mapgen_setting ("mcl_use_old_map_grid")
 
-if detected_old_maps then
-	use_old_map_grid = true
-	core.set_mapgen_setting ("mcl_use_old_map_grid", "true", true)
-elseif not use_old_map_grid then
-	use_old_map_grid = false
-	core.set_mapgen_setting ("mcl_use_old_map_grid", "false", true)
+if not use_old_map_grid then
+	if detected_old_maps then
+		use_old_map_grid = true
+		core.set_mapgen_setting ("mcl_use_old_map_grid", "true", true)
+	else
+		use_old_map_grid = false
+		core.set_mapgen_setting ("mcl_use_old_map_grid", "false", true)
+	end
 else
 	use_old_map_grid = core.is_yes (use_old_map_grid)
 end
