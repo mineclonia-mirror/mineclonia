8bf78d3e035f87701d8a66e4f7fd0057494369dd
short_salmon <short_salmon@noreply.codeberg.org>
2026-06-30 22:33:41 +0100

    Add relative coordinate functionality to setblock

--- mods/MISC/mcl_commands/setblock.lua
+++ mods/MISC/mcl_commands/setblock.lua
@@ -4,12 +4,17 @@ core.register_chatcommand("setblock", {
 	params = S("<X>,<Y>,<Z> <NodeString>"),
 	description = S("Set node at given position"),
 	privs = {give=true, interact=true},
-	func = function(_, param)
+	func = function(name, param)
+		local player = core.get_player_by_name(name)
+		local relpos
+		if player then
+			relpos = player:get_pos()
+		end
 		local p = {}
 		local nodestring
-		p.x, p.y, p.z, nodestring = param:match("^([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+) +(.+)$")
-		p.x, p.y, p.z = tonumber(p.x), tonumber(p.y), tonumber(p.z)
-		if p.x and p.y and p.z and nodestring then
+		p.x, p.y, p.z, nodestring = string.match(param, "^([%d.~-]+)[, ] *([%d.~-]+)[, ] *([%d.~-]+) +(.+)$")
+		p = core.parse_coordinates(p.x, p.y, p.z, relpos)
+		if p and p.x and p.y and p.z and nodestring then
 			if not core.registered_nodes[nodestring] then
 				return false, S("Invalid node")
 			end
