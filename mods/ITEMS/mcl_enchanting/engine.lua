e9a704bda4d92e372a903f665a3f0616c0548cf8
short_salmon <short_salmon@noreply.codeberg.org>
2026-07-10 16:34:36 +0100

    Remove now-unused injection of tool capability updates in after_use

--- mods/ITEMS/mcl_enchanting/engine.lua
+++ mods/ITEMS/mcl_enchanting/engine.lua
@@ -297,38 +297,6 @@ function mcl_enchanting.enchantments_snippet(_, _, itemstack)
 	end
 end
 
--- Returns the after_use callback function to use when registering an enchanted
--- item.  The after_use callback is used to update the tool_capabilities of
--- efficiency enchanted tools with outdated digging times.
---
--- It does this by calling apply_efficiency to reapply the efficiency
--- enchantment.  That function is written to use hash values to only update the
--- tool if neccessary.
---
--- This is neccessary for digging times of tools to be in sync when Mineclonia
--- or mods add new hardness values.
-local function get_after_use_callback(itemdef)
-	if itemdef.after_use then
-		-- If the tool already has an after_use, make sure to call that
-		-- one too.
-		return function(itemstack, user, node, digparams)
-			itemdef.after_use(itemstack, user, node, digparams)
-			--mcl_enchanting.update_groupcaps(itemstack, false)
-		end
-	end
-
-	-- If the tool does not have after_use, add wear to the tool as if no
-	-- after_use was registered.
-	return function(itemstack, user, _, digparams)
-		if not core.is_creative_enabled(user:get_player_name()) then
-			itemstack:add_wear(digparams.wear)
-		end
-
-		--local enchantments = mcl_enchanting.get_enchantments(itemstack)
-		--mcl_enchanting.update_groupcaps(itemstack, false)
-	end
-end
-
 function mcl_enchanting.initialize()
 	local register_tool_list = {}
 	local register_item_list = {}
@@ -352,7 +320,6 @@ function mcl_enchanting.initialize()
 			end
 
 			new_def._mcl_enchanting_enchanted_tool = new_name
-			new_def.after_use = get_after_use_callback(itemdef)
 			local register_list = register_item_list
 			if itemdef.type == "tool" then
 				register_list = register_tool_list
