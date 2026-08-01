ed25a2ccc77c5550c83287f11f9ac0741f834e1d
Fossphorus <fossphorus@noreply.codeberg.org>
2026-08-01 23:48:21 +0100

    Update the crafting table GUI

--- mods/ITEMS/mcl_crafting_table/init.lua
+++ mods/ITEMS/mcl_crafting_table/init.lua
@@ -4,36 +4,33 @@ local C = core.colorize
 mcl_crafting_table = {}
 
 mcl_crafting_table.formspec = table.concat({
-	"formspec_version[4]",
-	"size[11.75,10.425]",
+	-- The size of an inventory slot is 64x64 pixels, multiplied by the GUI scaling.
+	-- The below maths assume a GUI scaling of 1.5, which I have found to be the most "stable" scale.
 
-	"label[2.25,0.375;" .. F(C(mcl_formspec.label_color, S("Crafting"))) .. "]",
+	"formspec_version[8]", -- Supported by Luanti 5.10, the most recent version on the most recent Debian Stable release.
+	"size[7.33333333333,6.91666666667]", -- 704/96 = 7.33333333333 ; 664/96 = 6.91666666667
+	"image[0,0;10.6666666667,10.6666666667;crafting_table.png]", -- (256/176)*7.33333333333 = 10.6666666667 ; (256/166)*6.91666666667 = 10.6666666667
+	"bgcolor[;;#000000BB]", -- Minecraft has a slight gradient to its background darkening, so getting this perfect isn't currently possible.
+	"style_type[list;size=0.666666666667,0.666666666667;spacing=0.0833333333333,0.0833333333333]", -- 64/96 = 0.666666666667 ; 8/96 = 0.0833333333333 (No idea how to change colour of the list tiles.)
+	"style_type[button;bgimg=button.png;bgimg_hovered=button_highlighted.png;bgimg_middle=true;bgimg_pressed=button_highlighted.png;sound=mesecons_button_push]",
 
-	mcl_formspec.get_itemslot_bg_v4(2.25, 0.75, 3, 3),
-	"list[current_player;craft;2.25,0.75;3,3;]",
+	-- The crafting part of the crafting table GUI.
+	"label[1.20833333333,0.46875;" .. F(C("#404040", S("Crafting"))) .. "]", -- 116/96 = 1.20833333333 ; (24+21)/96 = 0.46875 (No idea how to get this stuff to valign bottom.)
 
-	"image[6.125,2;1.5,1;gui_crafting_arrow.png]",
+	"list[current_player;craft;1.25,0.708333333333;3,3;]", -- 120/96 = 1.25 ; 68/96 = 0.708333333333
+	"list[current_player;craftpreview;5.16666666667,1.45833333333;1,1;]", -- 496/96 = 5.16666666667 ; 140/96 = 1.45833333333
 
-	mcl_formspec.get_itemslot_bg_v4(8.2, 2, 1, 1, 0.2),
-	"list[current_player;craftpreview;8.2,2;1,1;]",
+	-- The inventory part of the crafting table GUI.
+	"label[0.333333333333,3.21875;" .. F(C("#404040", S("Inventory"))) .. "]", -- 32/96 = 0.333333333333 ;
 
-	"label[0.375,4.7;" .. F(C(mcl_formspec.label_color, S("Inventory"))) .. "]",
-
-	mcl_formspec.get_itemslot_bg_v4(0.375, 5.1, 9, 3),
-	"list[current_player;main;0.375,5.1;9,3;9]",
-
-	mcl_formspec.get_itemslot_bg_v4(0.375, 9.05, 9, 1),
-	"list[current_player;main;0.375,9.05;9,1;]",
+	"list[current_player;main;0.333333333333,3.5;9,3;9]", -- 32/96 = 0.333333333333 ; 336/96 = 3.5
+	"list[current_player;main;0.333333333333,5.91666666667;9,1;]", -- 32/96 = 0.333333333333 ; 568/96 = 5.91666666667
 
 	"listring[current_player;craft]",
 	"listring[current_player;main]",
 
-	--Crafting guide button
-	"image_button[0.325,1.95;1.1,1.1;craftguide_book.png;__mcl_craftguide;]",
-	"tooltip[__mcl_craftguide;" .. F(S("Recipe book")) .. "]",
-
-	"image_button[6.025,3.175;1,1;mcl_crafting_table_inv_fill.png;__mcl_crafting_fillgrid;]",
-	"tooltip[__mcl_crafting_fillgrid;" .. F(S("Fill Craft Grid")) .. "]",
+	-- The recipe book button.
+	"button[0.208333333333,1.41666666667;0.833333333333,0.75;__mcl_craftguide;]", -- 20/96 = 0.208333333333 ; 136/96 = 1.41666666667 & 80/96 = 0.833333333333 ; 72/96 = 0.75
 })
 
 function mcl_crafting_table.has_crafting_table(player)
