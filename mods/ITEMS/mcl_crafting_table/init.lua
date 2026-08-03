084dcdbdd9a2d386fca9a2551e62d49537a39a5e
Fossphorus <fossphorus@noreply.codeberg.org>
2026-08-03 17:59:09 +0100

    Implement a MClike GUI scaling system

--- mods/ITEMS/mcl_crafting_table/init.lua
+++ mods/ITEMS/mcl_crafting_table/init.lua
@@ -1,59 +1,71 @@
 local S = core.get_translator(core.get_current_modname())
 local F = core.formspec_escape
 local C = core.colorize
-local GS = (1.5/core.settings:get("gui_scaling"))/24
+local LSN = (1.5/core.settings:get("gui_scaling"))/96 -- Luanti Scaling Negator (Required to negate Luanti's GUI scaling, so that we can implement our own system instead.)
+local MCS = math.ceil(core.settings:get("gui_scaling")/0.375) -- Minecraft Scaling
 mcl_crafting_table = {}
 
-mcl_crafting_table.formspec = table.concat({
+mcl_crafting_table.formspec = table.concat({ -- NOTE: The numbers below correlate with the amount of pixels!
+
 	-- The formspec version.
+
 	"formspec_version[8]", -- Supported by Luanti 5.10, which is currently the latest version on Debian Stable.
 
 
 	-- Prepend default values.
-	"size["..(GS*176)..","..(GS*166).."]", -- The size of the formspec within Minecraft's default "crafting_table.png" texture, in pixels.
+
+	"size["..(LSN*176*MCS)..","..(LSN*166*MCS).."]", -- The size of the GUI within Minecraft's default "crafting_table.png" texture.
+
 	"no_prepend[]", -- Disable the default prepends, as we want to define our own prepends which are more accurate to Minecraft.
+
 	"bgcolor[;true;#000000BB]", -- How much the background darkens when the GUI is open. Minecraft has a gradient darkening, so this is not 100% accurate.
+
 	"listcolors[#0000;#FFFFFF75;#0000;#0000007F;#FFFFFFFF]", -- The colour of the inventory slots, and the tooltips of items when hovering the cursor over them.
-	"style_type[list;size="..(GS*16)..","..(GS*16)..";".."spacing="..(GS*2)..","..(GS*2).."]", -- The size and spacing of the inventory slots within the GUI, in pixels.
+
+	"style_type[list;".."size="..(LSN*16*MCS)..","..(LSN*16*MCS)..";".."spacing="..(LSN*2*MCS)..","..(LSN*2*MCS).."]", -- The size and spacing of the inventory slots within the GUI.
+
 	"style_type[button;border=false;bgimg=button.png;sound=mesecons_button_push]", -- The default state of the recipe book button.
 
 
 	-- The visual part of the GUI.
-	"image[0,0;"..(GS*256)..","..(GS*256)..";".."crafting_table.png".."]", -- The GUI background.
 
-	"image["..(GS*29)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:3,4" .."]", -- C
-	"image["..(GS*35)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:2,7" .."]", -- r
-	"image["..(GS*41)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:1,6" .."]", -- a
-	"image["..(GS*47)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:6,6" .."]", -- f
-	"image["..(GS*52)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:4,7" .."]", -- t
-	"image["..(GS*56)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,6" .."]", -- i
-	"image["..(GS*58)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
-	"image["..(GS*64)..","..(GS*6)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:7,6" .."]", -- g
+	"image[0,0;"..(LSN*256*MCS)..","..(LSN*256*MCS)..";".."crafting_table.png".."]", -- The size of Minecraft's default "crafting_table.png" texture.
+
+	"image["..(LSN*29*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:3,4" .."]", -- C
+	"image["..(LSN*35*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:2,7" .."]", -- r
+	"image["..(LSN*41*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:1,6" .."]", -- a
+	"image["..(LSN*47*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:6,6" .."]", -- f
+	"image["..(LSN*52*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:4,7" .."]", -- t
+	"image["..(LSN*56*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,6" .."]", -- i
+	"image["..(LSN*58*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
+	"image["..(LSN*64*MCS)..","..(LSN*6*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:7,6" .."]", -- g
 
 	"style[__mcl_craftguide:hovered,__mcl_craftguide:pressed;bgimg=button_highlighted.png]", -- The recipe book button.
 
-	"image["..(GS*8) ..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,4" .."]", -- I
-	"image["..(GS*12)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
-	"image["..(GS*18)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:6,7" .."]", -- v
-	"image["..(GS*24)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:5,6" .."]", -- e
-	"image["..(GS*30)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
-	"image["..(GS*36)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:4,7" .."]", -- t
-	"image["..(GS*40)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:15,6".."]", -- o
-	"image["..(GS*46)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:2,7" .."]", -- r
-	"image["..(GS*52)..","..(GS*72)..";"..(GS*8)..","..(GS*8)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,7" .."]", -- y
+	"image["..(LSN*8*MCS) ..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,4" .."]", -- I
+	"image["..(LSN*12*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
+	"image["..(LSN*18*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:6,7" .."]", -- v
+	"image["..(LSN*24*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:5,6" .."]", -- e
+	"image["..(LSN*30*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:14,6".."]", -- n
+	"image["..(LSN*36*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:4,7" .."]", -- t
+	"image["..(LSN*40*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:15,6".."]", -- o
+	"image["..(LSN*46*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:2,7" .."]", -- r
+	"image["..(LSN*52*MCS)..","..(LSN*72*MCS)..";"..(LSN*8*MCS)..","..(LSN*8*MCS)..";".."ascii.png^[colorize:#404040^[sheet:16x16:9,7" .."]", -- y
 
 
 	-- The functional part of the GUI.
-	"list[current_player;craft;"       ..(GS*30).. ","..(GS*17)..";3,3;0]", -- The 3x3 crafting grid.
-	"list[current_player;craftpreview;"..(GS*124)..","..(GS*35)..";1,1;0]", -- The preview of the crafting output.
 
-	"button["..(GS*5)..","..(GS*34)..";"..(GS*20)..","..(GS*18)..";__mcl_craftguide;]", -- The recipe book button.
+	"list[current_player;craft;"       ..(LSN*30*MCS).. ","..(LSN*17*MCS)..";3,3;0]", -- The 3x3 crafting grid.
+	"list[current_player;craftpreview;"..(LSN*124*MCS)..","..(LSN*35*MCS)..";1,1;0]", -- The preview of the crafting output.
 
-	"list[current_player;main;"..(GS*8)..","..(GS*84) ..";9,3;9]", -- The player's backpack inventory.
-	"list[current_player;main;"..(GS*8)..","..(GS*142)..";9,1;0]", -- The player's HUD inventory.
+	"button["..(LSN*5*MCS)..","..(LSN*34*MCS)..";"..(LSN*20*MCS)..","..(LSN*18*MCS)..";__mcl_craftguide;]", -- The recipe book button.
+
+	"list[current_player;main;"..(LSN*8*MCS)..","..(LSN*84*MCS) ..";9,3;9]", -- The player's backpack inventory.
+	"list[current_player;main;"..(LSN*8*MCS)..","..(LSN*142*MCS)..";9,1;0]", -- The player's HUD inventory.
 
 	"listring[current_player;craft]", -- This allows for shift-clicking functionality.
 	"listring[current_player;main]", -- This allows for shift-clicking functionality.
+
 })
 
 function mcl_crafting_table.has_crafting_table(player)
