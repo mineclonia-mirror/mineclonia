mineclonia-mirror/mineclonia

commit ca909bab02fb97d670dcdd8c84187ab325470808

Move vanilla wall registrations into their respective mods

mods/ITEMS/mcl_core/nodes_stairs.lua
@@ -20,17 +20,34 @@ mcl_stairs.register_stair_and_slab("andesite", {
 	description_slab = S("Andesite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:andesite"}},
 })
+mcl_walls.register_wall_def(":mcl_walls:andesite", {
+	description = S("Andesite Wall"),
+	source = "mcl_core:andesite",
+	_mcl_stonecutter_recipes = {"mcl_core:andesite"},
+})
+
 mcl_stairs.register_stair_and_slab("granite", {
 	baseitem = "mcl_core:granite",
 	description_stair = S("Granite Stairs"),
 	description_slab = S("Granite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:granite"}},
 })
+mcl_walls.register_wall_def(":mcl_walls:granite", {
+	description = S("Granite Wall"),
+	source = "mcl_core:granite",
+	_mcl_stonecutter_recipes = {"mcl_core:granite",},
+})
+
 mcl_stairs.register_stair_and_slab("diorite", {
 	baseitem = "mcl_core:diorite",
 	description_stair = S("Diorite Stairs"),
 	description_slab = S("Diorite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:diorite"}},
+})
+mcl_walls.register_wall_def(":mcl_walls:diorite", {
+	description = S("Diorite Wall"),
+	source = "mcl_core:diorite",
+	_mcl_stonecutter_recipes = {"mcl_core:diorite",},
 })
 
 mcl_stairs.register_stair_and_slab("cobble", {
@@ -39,11 +56,24 @@ mcl_stairs.register_stair_and_slab("cobble", {
 	description_slab = S("Cobblestone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:cobble", "mcl_core:stone"}},
 })
+mcl_walls.register_wall_def(":mcl_walls:cobble",{
+	source = "mcl_core:cobble",
+	description = S("Cobblestone Wall"),
+	tiles = {"mcl_walls_cobble_wall_top.png", "default_cobble.png", "mcl_walls_cobble_wall_side.png"},
+	_mcl_stonecutter_recipes = { "mcl_core:cobble", "mcl_core:stone" },
+})
+
 mcl_stairs.register_stair_and_slab("mossycobble", {
 	baseitem = "mcl_core:mossycobble",
 	description_stair = S("Mossy Cobblestone Stairs"),
 	description_slab = S("Mossy Cobblestone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:mossycobble"}},
+})
+mcl_walls.register_wall_def(":mcl_walls:mossycobble", {
+	source = "mcl_core:mossycobble",
+	description = S("Mossy Cobblestone Wall"),
+	tiles = {"mcl_walls_cobble_mossy_wall_top.png", "default_mossycobble.png", "mcl_walls_cobble_mossy_wall_side.png"},
+	_mcl_stonecutter_recipes = { "mcl_core:mossycobble" },
 })
 
 mcl_stairs.register_stair_and_slab("brick_block", {
@@ -52,7 +82,11 @@ mcl_stairs.register_stair_and_slab("brick_block", {
 	description_slab = S("Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:brick_block"}},
 })
-
+mcl_walls.register_wall_def(":mcl_walls:brick", {
+	description = S("Brick Wall"),
+	source = "mcl_core:brick_block",
+	_mcl_stonecutter_recipes = {"mcl_core:brick_block",},
+})
 
 mcl_stairs.register_stair_and_slab("sandstone", {
 	baseitem = "mcl_core:sandstone",
@@ -60,6 +94,12 @@ mcl_stairs.register_stair_and_slab("sandstone", {
 	description_slab = S("Sandstone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:sandstone"}},
 })
+mcl_walls.register_wall_def(":mcl_walls:sandstone", {
+	description = S("Sandstone Wall"),
+	source = "mcl_core:sandstone",
+	_mcl_stonecutter_recipes = {"mcl_core:sandstone",},
+})
+
 mcl_stairs.register_stair_and_slab("sandstonesmooth2", {
 	baseitem = "mcl_core:sandstonesmooth2",
 	description_stair = S("Smooth Sandstone Stairs"),
@@ -83,6 +123,11 @@ mcl_stairs.register_stair_and_slab("redsandstone", {
 	description_slab = S("Red Sandstone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:redsandstone"}},
 })
+mcl_walls.register_wall_def(":mcl_walls:redsandstone", {
+	description = S("Red Sandstone Wall"),
+	source = "mcl_core:redsandstone",
+	_mcl_stonecutter_recipes = {"mcl_core:redsandstone",},
+})
 mcl_stairs.register_stair_and_slab("redsandstonesmooth2", {
 	baseitem = "mcl_core:redsandstonesmooth2",
 	description_stair = S("Smooth Red Sandstone Stairs"),
@@ -105,6 +150,11 @@ mcl_stairs.register_stair_and_slab("stonebrick", {
 	description_stair = S("Stone Brick Stairs"),
 	description_slab = S("Stone Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:stone", "mcl_core:stonebrick"}},
+})
+mcl_walls.register_wall_def(":mcl_walls:stonebrick", {
+	description = S("Stone Brick Wall"),
+	source = "mcl_core:stonebrick",
+	_mcl_stonecutter_recipes = {"mcl_core:stonebrick", "mcl_core:stone"},
 })
 
 mcl_stairs.register_stair("andesite_smooth", {
@@ -148,4 +198,9 @@ mcl_stairs.register_stair_and_slab("stonebrickmossy", {
 	description_stair = S("Mossy Stone Brick Stairs"),
 	description_slab = S("Mossy Stone Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:stonebrickmossy"}},
+})
+mcl_walls.register_wall_def(":mcl_walls:stonebrickmossy", {
+	description = S("Mossy Stone Brick Wall"),
+	source = "mcl_core:stonebrickmossy",
+	_mcl_stonecutter_recipes = {"mcl_core:stonebrickmossy",},
 })

mods/ITEMS/mcl_end/building.lua
@@ -183,6 +183,12 @@ mcl_stairs.register_stair_and_slab("end_bricks", {
 	overrides = {_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"}},{_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"}}
 })
 
+mcl_walls.register_wall_def(":mcl_walls:endbricks", {
+	description = S("End Stone Brick Wall"),
+	source = "mcl_end:end_bricks",
+	_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"},
+})
+
 mcl_stairs.register_stair_and_slab("purpur_block", {
 	baseitem = "mcl_end:purpur_block",
 	description_stair = S("Purpur Stairs"),

mods/ITEMS/mcl_mud/init.lua
@@ -52,6 +52,12 @@ mcl_stairs.register_stair_and_slab("mud_brick", {
 	overrides = {_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks"}},{_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks"}}
 })
 
+mcl_walls.register_wall_def(":mcl_walls:mudbrick", {
+	description = S("Mud Brick Wall"),
+	source = "mcl_mud:mud_bricks",
+	_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks",},
+})
+
 -- packed mud
 core.register_craft({
 	type = "shapeless",

mods/ITEMS/mcl_nether/init.lua
@@ -313,11 +313,25 @@ mcl_stairs.register_stair_and_slab("nether_brick", {
 	description_slab = S("Nether Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = { "mcl_nether:nether_brick" }},{_mcl_stonecutter_recipes = { "mcl_nether:nether_brick" }},
 })
+
+mcl_walls.register_wall_def(":mcl_walls:netherbrick", {
+	description = S("Nether Brick Wall"),
+	source = "mcl_nether:nether_brick",
+	_mcl_stonecutter_recipes = {"mcl_nether:nether_brick",},
+})
+
+
 mcl_stairs.register_stair_and_slab("red_nether_brick", {
 	baseitem = "mcl_nether:red_nether_brick",
 	description_stair = S("Red Nether Brick Stairs"),
 	description_slab = S("Red Nether Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = { "mcl_nether:red_nether_brick" }},{_mcl_stonecutter_recipes = { "mcl_nether:red_nether_brick" }},
+})
+
+mcl_walls.register_wall_def(":mcl_walls:rednetherbrick", {
+	description = S("Red Nether Brick Wall"),
+	source = "mcl_nether:red_nether_brick",
+	_mcl_stonecutter_recipes = {"mcl_nether:red_nether_brick",},
 })
 
 -- Nether Brick Fence (without fence gate!)

mods/ITEMS/mcl_ocean/prismarine.lua
@@ -68,12 +68,19 @@ mcl_stairs.register_stair_and_slab("prismarine", {
 	description_slab = S("Prismarine Slab"),
 	overrides = {_mcl_stonecutter_recipes = { "mcl_ocean:prismarine" }},{_mcl_stonecutter_recipes = { "mcl_ocean:prismarine" }}
 })
+mcl_walls.register_wall_def(":mcl_walls:prismarine", {
+	description = S("Prismarine Wall"),
+	source = "mcl_ocean:prismarine",
+	_mcl_stonecutter_recipes = {"mcl_ocean:prismarine",},
+})
+
 mcl_stairs.register_stair_and_slab("prismarine_brick", {
 	baseitem = "mcl_ocean:prismarine_brick",
 	description_stair = S("Prismarine Brick Stairs"),
 	description_slab = S("Prismarine Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = { "mcl_ocean:prismarine_brick" }},{_mcl_stonecutter_recipes = { "mcl_ocean:prismarine_brick" }}
 })
+
 mcl_stairs.register_stair_and_slab("prismarine_dark", {
 	baseitem = "mcl_ocean:prismarine_dark",
 	description_stair = S("Dark Prismarine Stairs"),

mods/ITEMS/mcl_walls/init.lua
@@ -1,5 +1,4 @@
 local modname = core.get_current_modname()
-local modpath = core.get_modpath(modname)
 local S = core.get_translator(modname)
 
 mcl_walls = {}
@@ -285,8 +284,6 @@ function mcl_walls.register_wall_def(name,def)
 	def.source = nil
 	mcl_walls.register_wall(name, nil, source, nil, nil, nil, nil, def)
 end
-
-dofile(modpath.."/register.lua")
 
 core.register_on_placenode(update_wall_global)
 core.register_on_dignode(update_wall_global)

mods/ITEMS/mcl_core/mod.conf
@@ -1,3 +1,3 @@
 name = mcl_core
 description = Core items of Mineclonia: Basic biome blocks (dirt, sand, stones, etc.), derived items, glass, sugar cane, cactus, barrier, mining tools, hand, craftitems, and misc. items which don't really fit anywhere else.
-depends = mcl_autogroup, mcl_init, mcl_sounds, mcl_util, mcl_worlds, doc_items, mcl_enchanting, mcl_colors, mcl_stairs, mcl_fences, mcl_flowerpots, mcl_trees, mcl_dyes, mcl_player, doc, mcl_aliases, mcl_liquids, screwdriver
+depends = mcl_autogroup, mcl_init, mcl_sounds, mcl_util, mcl_worlds, doc_items, mcl_enchanting, mcl_colors, mcl_stairs, mcl_fences, mcl_walls, mcl_flowerpots, mcl_trees, mcl_dyes, mcl_player, doc, mcl_aliases, mcl_liquids, screwdriver

mods/ITEMS/mcl_end/mod.conf
@@ -1,2 +1,2 @@
 name = mcl_end
-depends = screwdriver, mcl_sounds, mcl_util, doc_items, mcl_worlds, mcl_structures, mcl_stairs, mcl_levelgen
+depends = screwdriver, mcl_sounds, mcl_util, doc_items, mcl_worlds, mcl_structures, mcl_stairs, mcl_walls, mcl_levelgen

commit 92de0f07c099fb4099e5ac81103b85c15194423a

Fix wall drop regression after ca909bab02
The regression was in passing the new node names directly with a colon,
which allowed registering the wall nodes under the old unified
modspace, but trickled its way down fields like `drop` through the
older depinjection function. Now `mcl_walls` just puts the colon prefix
by itself, consistent with other node APIs.

mods/ITEMS/mcl_core/nodes_stairs.lua
@@ -20,7 +20,7 @@ mcl_stairs.register_stair_and_slab("andesite", {
 	description_slab = S("Andesite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:andesite"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:andesite", {
+mcl_walls.register_wall_def("mcl_walls:andesite", {
 	description = S("Andesite Wall"),
 	source = "mcl_core:andesite",
 	_mcl_stonecutter_recipes = {"mcl_core:andesite"},
@@ -32,7 +32,7 @@ mcl_stairs.register_stair_and_slab("granite", {
 	description_slab = S("Granite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:granite"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:granite", {
+mcl_walls.register_wall_def("mcl_walls:granite", {
 	description = S("Granite Wall"),
 	source = "mcl_core:granite",
 	_mcl_stonecutter_recipes = {"mcl_core:granite",},
@@ -44,7 +44,7 @@ mcl_stairs.register_stair_and_slab("diorite", {
 	description_slab = S("Diorite Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:diorite"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:diorite", {
+mcl_walls.register_wall_def("mcl_walls:diorite", {
 	description = S("Diorite Wall"),
 	source = "mcl_core:diorite",
 	_mcl_stonecutter_recipes = {"mcl_core:diorite",},
@@ -56,7 +56,7 @@ mcl_stairs.register_stair_and_slab("cobble", {
 	description_slab = S("Cobblestone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:cobble", "mcl_core:stone"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:cobble",{
+mcl_walls.register_wall_def("mcl_walls:cobble",{
 	source = "mcl_core:cobble",
 	description = S("Cobblestone Wall"),
 	tiles = {"mcl_walls_cobble_wall_top.png", "default_cobble.png", "mcl_walls_cobble_wall_side.png"},
@@ -69,7 +69,7 @@ mcl_stairs.register_stair_and_slab("mossycobble", {
 	description_slab = S("Mossy Cobblestone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:mossycobble"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:mossycobble", {
+mcl_walls.register_wall_def("mcl_walls:mossycobble", {
 	source = "mcl_core:mossycobble",
 	description = S("Mossy Cobblestone Wall"),
 	tiles = {"mcl_walls_cobble_mossy_wall_top.png", "default_mossycobble.png", "mcl_walls_cobble_mossy_wall_side.png"},
@@ -82,7 +82,7 @@ mcl_stairs.register_stair_and_slab("brick_block", {
 	description_slab = S("Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:brick_block"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:brick", {
+mcl_walls.register_wall_def("mcl_walls:brick", {
 	description = S("Brick Wall"),
 	source = "mcl_core:brick_block",
 	_mcl_stonecutter_recipes = {"mcl_core:brick_block",},
@@ -94,7 +94,7 @@ mcl_stairs.register_stair_and_slab("sandstone", {
 	description_slab = S("Sandstone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:sandstone"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:sandstone", {
+mcl_walls.register_wall_def("mcl_walls:sandstone", {
 	description = S("Sandstone Wall"),
 	source = "mcl_core:sandstone",
 	_mcl_stonecutter_recipes = {"mcl_core:sandstone",},
@@ -123,7 +123,7 @@ mcl_stairs.register_stair_and_slab("redsandstone", {
 	description_slab = S("Red Sandstone Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:redsandstone"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:redsandstone", {
+mcl_walls.register_wall_def("mcl_walls:redsandstone", {
 	description = S("Red Sandstone Wall"),
 	source = "mcl_core:redsandstone",
 	_mcl_stonecutter_recipes = {"mcl_core:redsandstone",},
@@ -151,7 +151,7 @@ mcl_stairs.register_stair_and_slab("stonebrick", {
 	description_slab = S("Stone Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:stone", "mcl_core:stonebrick"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:stonebrick", {
+mcl_walls.register_wall_def("mcl_walls:stonebrick", {
 	description = S("Stone Brick Wall"),
 	source = "mcl_core:stonebrick",
 	_mcl_stonecutter_recipes = {"mcl_core:stonebrick", "mcl_core:stone"},
@@ -199,7 +199,7 @@ mcl_stairs.register_stair_and_slab("stonebrickmossy", {
 	description_slab = S("Mossy Stone Brick Slab"),
 	overrides = {_mcl_stonecutter_recipes = {"mcl_core:stonebrickmossy"}},
 })
-mcl_walls.register_wall_def(":mcl_walls:stonebrickmossy", {
+mcl_walls.register_wall_def("mcl_walls:stonebrickmossy", {
 	description = S("Mossy Stone Brick Wall"),
 	source = "mcl_core:stonebrickmossy",
 	_mcl_stonecutter_recipes = {"mcl_core:stonebrickmossy",},

mods/ITEMS/mcl_end/building.lua
@@ -183,7 +183,7 @@ mcl_stairs.register_stair_and_slab("end_bricks", {
 	overrides = {_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"}},{_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"}}
 })
 
-mcl_walls.register_wall_def(":mcl_walls:endbricks", {
+mcl_walls.register_wall_def("mcl_walls:endbricks", {
 	description = S("End Stone Brick Wall"),
 	source = "mcl_end:end_bricks",
 	_mcl_stonecutter_recipes = {"mcl_end:end_bricks","mcl_end:end_stone"},

mods/ITEMS/mcl_mud/init.lua
@@ -52,7 +52,7 @@ mcl_stairs.register_stair_and_slab("mud_brick", {
 	overrides = {_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks"}},{_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks"}}
 })
 
-mcl_walls.register_wall_def(":mcl_walls:mudbrick", {
+mcl_walls.register_wall_def("mcl_walls:mudbrick", {
 	description = S("Mud Brick Wall"),
 	source = "mcl_mud:mud_bricks",
 	_mcl_stonecutter_recipes = {"mcl_mud:mud_bricks",},

mods/ITEMS/mcl_nether/init.lua
@@ -314,7 +314,7 @@ mcl_stairs.register_stair_and_slab("nether_brick", {
 	overrides = {_mcl_stonecutter_recipes = { "mcl_nether:nether_brick" }},{_mcl_stonecutter_recipes = { "mcl_nether:nether_brick" }},
 })
 
-mcl_walls.register_wall_def(":mcl_walls:netherbrick", {
+mcl_walls.register_wall_def("mcl_walls:netherbrick", {
 	description = S("Nether Brick Wall"),
 	source = "mcl_nether:nether_brick",
 	_mcl_stonecutter_recipes = {"mcl_nether:nether_brick",},
@@ -328,7 +328,7 @@ mcl_stairs.register_stair_and_slab("red_nether_brick", {
 	overrides = {_mcl_stonecutter_recipes = { "mcl_nether:red_nether_brick" }},{_mcl_stonecutter_recipes = { "mcl_nether:red_nether_brick" }},
 })
 
-mcl_walls.register_wall_def(":mcl_walls:rednetherbrick", {
+mcl_walls.register_wall_def("mcl_walls:rednetherbrick", {
 	description = S("Red Nether Brick Wall"),
 	source = "mcl_nether:red_nether_brick",
 	_mcl_stonecutter_recipes = {"mcl_nether:red_nether_brick",},

mods/ITEMS/mcl_ocean/prismarine.lua
@@ -68,7 +68,7 @@ mcl_stairs.register_stair_and_slab("prismarine", {
 	description_slab = S("Prismarine Slab"),
 	overrides = {_mcl_stonecutter_recipes = { "mcl_ocean:prismarine" }},{_mcl_stonecutter_recipes = { "mcl_ocean:prismarine" }}
 })
-mcl_walls.register_wall_def(":mcl_walls:prismarine", {
+mcl_walls.register_wall_def("mcl_walls:prismarine", {
 	description = S("Prismarine Wall"),
 	source = "mcl_ocean:prismarine",
 	_mcl_stonecutter_recipes = {"mcl_ocean:prismarine",},

mods/ITEMS/mcl_walls/init.lua
@@ -167,7 +167,7 @@ function mcl_walls.register_wall(nodename, description, source, tiles, inventory
 		if i == 15 or i == 0 then need_pillar = true end
 		if need_pillar then table.insert(take, pillar) end
 
-		core.register_node(nodename.."_"..i, table.merge({
+		core.register_node(":"..nodename.."_"..i, table.merge({
 			collision_box = {
 				type = "fixed",
 				fixed = {-4/16, -0.5, -4/16, 4/16, 1, 4/16}
@@ -194,7 +194,7 @@ function mcl_walls.register_wall(nodename, description, source, tiles, inventory
 		doc.add_entry_alias("nodes", nodename, "nodes", nodename.."_"..i)
 	end
 
-	core.register_node(nodename.."_16", table.merge({
+	core.register_node(":"..nodename.."_16", table.merge({
 		drawtype = "nodebox",
 		collision_box = {
 				type = "fixed",
@@ -219,7 +219,7 @@ function mcl_walls.register_wall(nodename, description, source, tiles, inventory
 	}, overrides or {}))
 	doc.add_entry_alias("nodes", nodename, "nodes", nodename.."_16")
 
-	core.register_node(nodename.."_21", table.merge({
+	core.register_node(":"..nodename.."_21", table.merge({
 		drawtype = "nodebox",
 		collision_box = {
 				type = "fixed",
@@ -244,7 +244,7 @@ function mcl_walls.register_wall(nodename, description, source, tiles, inventory
 	doc.add_entry_alias("nodes", nodename, "nodes", nodename.."_21")
 
 	-- Inventory item
-	core.register_node(nodename, table.merge({
+	core.register_node(":"..nodename, table.merge({
 		description = description,
 		_doc_items_longdesc = S("A piece of wall. It cannot be jumped over with a simple jump. When multiple of these are placed to next to each other, they will automatically build a nice wall structure."),
 		paramtype = "light",