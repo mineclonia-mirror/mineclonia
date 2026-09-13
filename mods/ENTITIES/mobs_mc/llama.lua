c726b01c2b2e6222e0f8331cf2b5b51d15006567
Fhelron <fhelron@danielschlep.de>
2026-09-13 14:57:19 +0200

    remove unused space line

--- mods/ENTITIES/mobs_mc/llama.lua
+++ mods/ENTITIES/mobs_mc/llama.lua
@@ -547,7 +547,6 @@ local llama_spit = {
 function llama_spit:hit_object (object)
 	local damage = 1
 	if object:is_player() then
-
 		local dot_attack = mcl_shields.find_angle(self._prev_pos, object)
 		local can_block, stack = mcl_shields.can_block(object, dot_attack)
 		if can_block then
