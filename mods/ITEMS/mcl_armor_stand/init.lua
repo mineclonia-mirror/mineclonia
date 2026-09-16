7e0126b0e398150782b15fb307c5234ebbbf0cdb
amino <amino@noreply.codeberg.org>
2026-09-17 00:07:29 +0200

    Move legacy armor stand entity handling to COMPAT

--- mods/ITEMS/mcl_armor_stand/init.lua
+++ mods/ITEMS/mcl_armor_stand/init.lua
@@ -211,12 +211,3 @@ core.register_craft({
 		{"mcl_core:stick", "mcl_stairs:slab_stone", "mcl_core:stick"},
 	}
 })
-
--- Legacy handling
-core.register_entity(":3d_armor_stand:armor_entity", {
-	on_activate = function(self)
-		core.log("action", "[mcl_armor_stand] Removing legacy entity: 3d_armor_stand:armor_entity")
-		self.object:remove()
-	end,
-	static_save = false,
-})
