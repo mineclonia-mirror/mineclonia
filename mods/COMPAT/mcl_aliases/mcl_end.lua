b1520ecef2239bbb5623c6b80724655eb9f5b162
amino <amino@noreply.codeberg.org>
2026-09-17 20:20:59 +0200

    Move mcl_end alias to mcl_aliases

---
+++ mods/COMPAT/mcl_aliases/mcl_end.lua
@@ -0,0 +1,1 @@
+core.register_alias("mcl_end_crystal:end_crystal", "mcl_end:crystal")
--- mods/ITEMS/mcl_end/end_crystal.lua
+++ mods/ITEMS/mcl_end/end_crystal.lua
@@ -231,7 +231,6 @@ core.register_craft({
 	}
 })
 
-core.register_alias("mcl_end_crystal:end_crystal", "mcl_end:crystal")
 
 ------------------------------------------------------------------------
 -- Dragon resurrection.
